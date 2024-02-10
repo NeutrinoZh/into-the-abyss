f1ebb3933d8b00fae35cb13e6d5fcb64c0b606cf
NeutrinoZh <neutrinozh@gmail.com>
2024-02-10 19:38:53 +0200

    added left&right borders for swipes

--- Assets/IntoTheAbyss/Scripts/Player/PlayerController.cs
+++ Assets/IntoTheAbyss/Scripts/Player/PlayerController.cs
@@ -4,6 +4,13 @@ using UnityEngine.InputSystem;
 
 namespace IntoTheAbyss.Game {
     public class PlayerController : MonoBehaviour {
+
+        [SerializeField] Vector3 m_step;
+        [SerializeField] private float m_sensitivity;
+
+        [SerializeField] private float m_leftBorder;
+        [SerializeField] private float m_rightBorder;
+
         private PlayerInput m_input;
         private bool m_can_swap = true;
 
@@ -19,13 +26,17 @@ namespace IntoTheAbyss.Game {
         private void Update() {
             Vector2 swipeDelta = m_input.Input.Player.Swipe.ReadValue<Vector2>();
 
-            if (swipeDelta.x > 100 && m_can_swap) {
-                transform.position += new Vector3(1.5f, 0, 0);
+            if (swipeDelta.x > m_sensitivity && m_can_swap) {
+                if (transform.position.x < m_rightBorder)
+                    transform.position += m_step;
+
                 m_can_swap = false;
             }
 
-            if (swipeDelta.x < -100 && m_can_swap) {
-                transform.position += new Vector3(-1.5f, 0, 0);
+            if (swipeDelta.x < -m_sensitivity && m_can_swap) {
+                if (transform.position.x > m_leftBorder)
+                    transform.position -= m_step;
+
                 m_can_swap = false;
             }
         }
