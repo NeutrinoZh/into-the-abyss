7f314d9fb945bae54bf15daa150256d6e2d58a2a
NeutrinoZh <neutrinozh@gmail.com>
2024-02-10 20:14:25 +0200

    added camera controller

---
+++ Assets/IntoTheAbyss/Scripts/Camera/CameraController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace IntoTheAbyss {
+    public class CameraController : MonoBehaviour {
+        [SerializeField] private Transform m_target;
+        [SerializeField] private Vector3 m_offset;
+
+        const float c_cameraSpeed = 30f;
+
+        private void Update() {
+            Vector3 direction =
+                c_cameraSpeed * Time.deltaTime *
+                (m_target.position + m_offset - transform.position).normalized;
+            direction.z = direction.x = 0;
+
+            transform.position += direction;
+        }
+    }
+}
