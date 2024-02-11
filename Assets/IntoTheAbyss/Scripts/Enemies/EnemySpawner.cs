0c5e137c79509969292877c0ebde3f32599d58dc
NeutrinoZh <neutrinozh@gmail.com>
2024-02-11 11:34:27 +0200

    added middle spike

--- Assets/IntoTheAbyss/Scripts/Enemies/EnemySpawner.cs
+++ Assets/IntoTheAbyss/Scripts/Enemies/EnemySpawner.cs
@@ -15,32 +15,38 @@ namespace IntoTheAbyss.Game {
 
         private void SpawnEnemy() {
             EnemyType type = (EnemyType)Random.Range(0, 3);
+            var enemy = EnemyManager.Instance.SpawnEnemy(type);
+
             switch (type) {
                 case EnemyType.SIDE_SPIKE:
-                    SpawnSideSpike(false);
+                    SpawnSideSpike(enemy);
                     break;
                 case EnemyType.SIDE_LONG_SPIKE:
-                    SpawnSideSpike(true);
+                    SpawnSideSpike(enemy);
+                    break;
+                case EnemyType.MIDDLE_SPIKE:
+                    SpawnMiddleSpike(enemy);
                     break;
                 default:
                     break;
             }
-
-        }
-
-        private void SpawnSideSpike(bool isLong) {
-            var enemy = EnemyManager.Instance.SpawnEnemy(isLong ? EnemyType.SIDE_LONG_SPIKE : EnemyType.SIDE_SPIKE);
-
-            var isRight = Random.Range(0, 2) == 0;
-            enemy.transform.position = m_spawnPoints[isRight ? ^1 : 0].position;
-
-            enemy.GetComponent<SpriteRenderer>().flipY = isRight;
 
             m_enemies.Add(enemy);
             if (m_enemies.Count > 5) {
                 EnemyManager.Instance.DestroyEnemy(m_enemies[0]);
                 m_enemies.Remove(m_enemies[0]);
             }
+        }
+
+        private void SpawnMiddleSpike(Transform _enemy) {
+            var spawnpoint = m_spawnPoints[1];
+            _enemy.transform.position = spawnpoint.position;
+        }
+
+        private void SpawnSideSpike(Transform _enemy) {
+            var isRight = Random.Range(0, 2) == 0;
+            _enemy.transform.position = m_spawnPoints[isRight ? ^1 : 0].position;
+            _enemy.GetComponent<SpriteRenderer>().flipY = isRight;
         }
     }
 }
