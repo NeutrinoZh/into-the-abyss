b84a548381163f65d010ab7877c710b36205b902
NeutrinoZh <neutrinozh@gmail.com>
2024-02-11 10:53:39 +0200

    enemy pool

--- Assets/IntoTheAbyss/Scripts/Enemies/EnemyManager.cs
+++ Assets/IntoTheAbyss/Scripts/Enemies/EnemyManager.cs
@@ -1,6 +1,5 @@
-using System.Collections.Generic;
-
 using UnityEngine;
+using UnityEngine.Pool;
 
 namespace IntoTheAbyss.Game {
     public class EnemyManager : MonoBehaviour {
@@ -14,13 +13,41 @@ namespace IntoTheAbyss.Game {
                 Destroy(gameObject);
             } else
                 Instance = this;
+
+            PoolInit();
         }
         // ==============================================//
 
-        private readonly List<Transform> m_enemies = new();
 
-        public void AddEnemy(Transform _enemy) {
-            m_enemies.Add(_enemy);
+        [SerializeField] private Transform m_enemyPrefab;
+        private ObjectPool<GameObject> m_enemies_pool;
+
+        private void PoolInit() {
+            m_enemies_pool = new(
+                () => Instantiate(m_enemyPrefab.gameObject, transform),
+                obj => obj.SetActive(true),
+                obj => obj.SetActive(false),
+                obj => Destroy(obj),
+                false, 10, 10
+            );
+        }
+
+        public Transform SpawnEnemy(EnemyType _type) {
+            var enemy = m_enemies_pool.Get().transform;
+
+            var child = enemy.transform.GetChild((int)_type);
+            child.gameObject.SetActive(true);
+
+            return child;
+        }
+
+        public void DestroyEnemy(Transform _enemy) {
+            _enemy = _enemy.parent;
+
+            foreach (Transform chlid in _enemy.transform)
+                chlid.gameObject.SetActive(false);
+
+            m_enemies_pool.Release(_enemy.gameObject);
         }
     }
 }
--- Assets/IntoTheAbyss/Scripts/Enemies/EnemySpawner.cs
+++ Assets/IntoTheAbyss/Scripts/Enemies/EnemySpawner.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 namespace IntoTheAbyss.Game {
     public class EnemySpawner : MonoBehaviour {
-        [SerializeField] private Transform m_spikePrefab;
         [SerializeField] private List<Transform> m_spawnPoints;
+        private readonly List<Transform> m_enemies = new() { };
 
 
         private void Start() {
@@ -13,18 +13,19 @@ namespace IntoTheAbyss.Game {
             fallingController.OnPerSection += SpawnEnemy;
         }
 
-        private void OnDestroy() {
-            var fallingController = Player.Instance.GetComponent<FallingController>();
-            fallingController.OnPerSection -= SpawnEnemy;
-        }
-
         private void SpawnEnemy() {
-            var enemy = Instantiate(m_spikePrefab, EnemyManager.Instance.transform);
+            var enemy = EnemyManager.Instance.SpawnEnemy(EnemyType.SIDE_SPIKE);
 
             var isRight = Random.Range(0, 2) == 0;
             enemy.transform.position = m_spawnPoints[isRight ? ^1 : 0].position;
 
             enemy.GetComponent<SpriteRenderer>().flipY = isRight;
+
+            m_enemies.Add(enemy);
+            if (m_enemies.Count > 5) {
+                EnemyManager.Instance.DestroyEnemy(m_enemies[0]);
+                m_enemies.Remove(m_enemies[0]);
+            }
         }
     }
 }
