b922449c5b5339fc2ed923400a2d6565faabfcf8
NeutrinoZh <neutrinozh@gmail.com>
2024-02-11 13:27:55 +0200

    die controller

---
+++ Assets/IntoTheAbyss/Scripts/Player/DieController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace IntoTheAbyss.Game {
+    public class DieController : MonoBehaviour {
+        private const string c_enemyTag = "Enemy";
+
+        private void OnCollisionEnter2D(Collision2D _other) {
+            if (_other.transform.CompareTag(c_enemyTag))
+                Time.timeScale = 0;
+        }
+    }
+}
