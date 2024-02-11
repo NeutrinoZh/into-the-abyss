548294418c91d11efb0e18693e313a1628f8a522
NeutrinoZh <neutrinozh@gmail.com>
2024-02-11 13:56:53 +0200

    menu ui

--- Assets/IntoTheAbyss/Scripts/Player/DieController.cs
+++ Assets/IntoTheAbyss/Scripts/Player/DieController.cs
@@ -1,12 +1,19 @@
+using System;
+
 using UnityEngine;
 
 namespace IntoTheAbyss.Game {
     public class DieController : MonoBehaviour {
+        public event Action OnDie;
+
         private const string c_enemyTag = "Enemy";
 
         private void OnCollisionEnter2D(Collision2D _other) {
-            if (_other.transform.CompareTag(c_enemyTag))
-                Time.timeScale = 0;
+            if (!_other.transform.CompareTag(c_enemyTag))
+                return;
+
+            Time.timeScale = 0;
+            OnDie?.Invoke();
         }
     }
 }
