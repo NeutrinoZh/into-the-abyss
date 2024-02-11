b9606bc6b35264ecabb05b9e9833f42352b4c2f0
NeutrinoZh <neutrinozh@gmail.com>
2024-02-11 12:09:58 +0200

    hud

--- Assets/IntoTheAbyss/Scripts/SessionManager.cs
+++ Assets/IntoTheAbyss/Scripts/SessionManager.cs
@@ -1,8 +1,32 @@
+using System;
+
 using UnityEngine;
 
 namespace IntoTheAbyss.Game {
     public class SessionManager : MonoBehaviour {
-        public int Score { get; private set; }
+        //==============================================//
+        // Singlton
+        public static SessionManager Instance { get; private set; }
+
+        private void Awake() {
+            if (Instance) {
+                Debug.LogWarning("Only one instance of SessionManager can be instantinate");
+                Destroy(gameObject);
+            } else
+                Instance = this;
+        }
+        // ==============================================//
+
+        public event Action OnChangeScore = null;
+
+        private int m_score;
+        public int Score {
+            get => m_score;
+            private set {
+                m_score = value;
+                OnChangeScore?.Invoke();
+            }
+        }
 
         private void Start() {
             var fallingController = Player.Instance.GetComponent<FallingController>();
