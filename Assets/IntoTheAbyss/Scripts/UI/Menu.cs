e19b0a2162674795aef73ab3e2cd5e2206724137
NeutrinoZh <neutrinozh@gmail.com>
2024-02-16 23:23:43 +0200

    main menu screen

--- Assets/IntoTheAbyss/Scripts/UI/Menu.cs
+++ Assets/IntoTheAbyss/Scripts/UI/Menu.cs
@@ -5,27 +5,27 @@ using IntoTheAbyss.Game;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-namespace IntoTheAbyss.UI {
-    public class Menu : MonoBehaviour {
+namespace IntoTheAbyss.UI
+{
+    public class Menu : MonoBehaviour
+    {
         public event Action OnRetry;
 
         private UIDocument m_document;
         private Label m_score;
         private Label m_highScore;
-        private Label m_gameOver;
         private Button m_retry;
 
         private string m_scorePattern;
         private string m_highScorePattern;
 
-        private const string c_scoreId = "Score";
-        private const string c_highScoreId = "HighScore";
-        private const string c_retryId = "Retry";
-        private const string c_gameOverId = "GameOver";
+        private const string c_scoreId = "score__label";
+        private const string c_highScoreId = "high-score__label";
+        private const string c_retryId = "retry__button";
 
-        private void Awake() {
+        private void Awake()
+        {
             m_document = GetComponent<UIDocument>();
-            m_gameOver = m_document.rootVisualElement.Query<Label>(c_gameOverId);
             m_score = m_document.rootVisualElement.Query<Label>(c_scoreId);
             m_highScore = m_document.rootVisualElement.Query<Label>(c_highScoreId);
             m_retry = m_document.rootVisualElement.Query<Button>(c_retryId);
@@ -38,25 +38,26 @@ namespace IntoTheAbyss.UI {
             ShowPlay();
         }
 
-        private void ShowPlay() {
+        private void ShowPlay()
+        {
             m_retry.text = "Play";
             m_score.visible = false;
-            m_gameOver.visible = false;
             m_highScore.text = m_highScorePattern.Replace("{}", SessionManager.Instance.HighScore.ToString());
         }
 
-        public void Show() {
+        public void Show()
+        {
             m_document.rootVisualElement.style.display = DisplayStyle.Flex;
 
             m_retry.text = "Retry";
             m_score.visible = true;
-            m_gameOver.visible = true;
 
             m_score.text = m_scorePattern.Replace("{}", SessionManager.Instance.Score.ToString());
             m_highScore.text = m_highScorePattern.Replace("{}", SessionManager.Instance.HighScore.ToString());
         }
 
-        public void Hide() {
+        public void Hide()
+        {
             m_document.rootVisualElement.style.display = DisplayStyle.None;
         }
     }
