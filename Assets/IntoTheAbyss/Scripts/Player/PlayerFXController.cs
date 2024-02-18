6fed4bbc9ae53a466eee059a160e74a8b58f1250
NeutrinoZh <neutrinozh@gmail.com>
2024-02-18 16:43:23 +0200

    player events

--- Assets/IntoTheAbyss/Scripts/Player/PlayerFXController.cs
+++ Assets/IntoTheAbyss/Scripts/Player/PlayerFXController.cs
@@ -4,14 +4,16 @@ using UnityEngine.VFX;
 namespace IntoTheAbyss.Game {
     public class PlayerFXController : MonoBehaviour {
         private const string c_eatFXName = "EatFX";
+
+        [SerializeField] private AudioClip m_crunch;
+        [SerializeField] private AudioClip m_slide;
+
         private VisualEffect m_eatFX;
         private AudioSource m_audioSource;
 
         private void Start() {
-            var fxObj = transform.Find(c_eatFXName);
-
-            m_eatFX = fxObj.GetComponent<VisualEffect>();
-            m_audioSource = fxObj.GetComponent<AudioSource>();
+            m_audioSource = GetComponent<AudioSource>();
+            m_eatFX = transform.Find(c_eatFXName).GetComponent<VisualEffect>();
 
             var eatController = GetComponent<EatController>();
             eatController.OnEat += PlayEatFX;
@@ -19,6 +21,8 @@ namespace IntoTheAbyss.Game {
 
         private void PlayEatFX() {
             m_eatFX.Play();
+
+            m_audioSource.clip = m_crunch;
             m_audioSource.Play();
         }
     }
